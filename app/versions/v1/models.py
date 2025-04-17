1f1a0984b078e1e137f56f7c61c50b5e586bad5e
cassandwich <cassandra.chanen@gmail.com>
2025-04-17 15:19:53 -0400

    Modified to align with list format lat/lon

--- app/versions/v1/models.py
+++ app/versions/v1/models.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
-from pydantic import field_validator, model_validator
+from pydantic import field_validator, model_validator, validator
 from sqlmodel import Field, SQLModel
+from sqlalchemy import JSON
+import ast
 from fastapi import FastAPI
-from typing import List
+from typing import List, Optional
+
 
 app = FastAPI(version="1")
-
 
 class DataRequestBase(SQLModel):
     """
@@ -37,8 +39,8 @@ class DataRequestBase(SQLModel):
     @model_validator(mode="before")
     def validate_location(cls, values):
         """Ensure list lengths match and check latitude/longitude validity."""
-        lat = values.__dict__.get("latitude")
-        lon = values.__dict__.get("longitude")
+        lat = ast.literal_eval(values.__dict__.get("latitude"))
+        lon = ast.literal_eval(values.__dict__.get("longitude"))
         file = values.__dict__.get("myFile")
         """Ensure list lengths match."""
         #if len(latitude) != len(longitude):
@@ -47,10 +49,12 @@ class DataRequestBase(SQLModel):
         if lat is None and file is None:
             raise ValueError("Must include either GeoJSON file or manually inputted latitude and longitudes")
         """Check latitude and longitude ranges"""
-        if lat > 90 or lat < -90:
-           raise ValueError("Latitudes must be between -90 and 90 degrees")
-        if lon > 180 or lon < -180:
-           raise ValueError("Latitudes must be between -90 and 90 degrees")
+        for i in lat:
+            if i > 90 or i < -90:
+                raise ValueError("Latitudes must be between -90 and 90 degrees")
+        for i in lon:
+            if float(i) > 180 or float(i) < -180:
+                raise ValueError("Latitudes must be between -90 and 90 degrees")
         return values
 
 class DataRequest(DataRequestBase, table=True):
@@ -64,15 +68,15 @@ class DataRequest(DataRequestBase, table=True):
     username: str
     title: str
     desc: str | None
-    fname: str
-    lname: str
-    email: str
+    authorFNames: str
+    authorLNames: str
+    authorEmails: str
     geometry: str
     latitude: str | None
     longitude: str | None
     myFile: str | None
-    start_date: datetime
-    end_date: datetime
+    start_date: str | None
+    end_date: str | None
     variables: str | None
     models: str | None
     path: str
@@ -93,15 +97,15 @@ class DataRequestPublic(DataRequestBase):
     username: str
     title: str
     desc: str | None
-    fname: str
-    lname: str
-    email: str
+    authorFNames: str
+    authorLNames: str
+    authorEmails: str
     geometry: str
-    latitude: str  | None
-    longitude: str  | None
+    latitude: str | None
+    longitude: str | None
     myFile: str | None
-    start_date: datetime
-    end_date: datetime
+    start_date: str | None
+    end_date: str | None
     variables: str | None
     models: str | None
     path: str
@@ -120,15 +124,15 @@ class DataRequestUpdate(DataRequestBase):
     username: str | None = None
     title: str | None = None
     desc: str | None = None
-    fname: str | None = None
-    lname: str | None = None
-    email: str | None = None
+    authorFNames: str | None = None
+    authorLNames: str | None = None
+    authorEmails: str | None = None
     geometry: str | None = None
     latitude: str | None = None
     longitude: str | None = None
     myFile: str | None = None
-    start_date: datetime | None = None
-    end_date: datetime | None = None
+    start_date: str | None = None
+    end_date: str | None = None
     variables: str | None = None
     models: str | None = None
     path: str | None = None
